66a5f66a7a473dd0adf36d392c8a93c2045199d4
Shafayet Hossain Shafin <shshafin999@gmail.com>
2025-01-28 22:42:20 +0600

    operation in CPP

--- first.cpp
+++ first.cpp
@@ -1,12 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main () {
-	int n; cin >> n;
-	cout << n << '\n';
-
-	cout << "shafayet Hossain shafin" << '\n';
+	long long int x = 0;
+	for (int i = 0; i < 1e10; i++) {
+		x++;
+		if (i == 1e6) cout << "total 10 lakh operation done" << '\n';
+		if (i == 1e7) cout << "total 1 koti operation done" << '\n';
+		if (i == 1e8) cout << "total 10 koti operation done" << '\n';
+		if (i == 1e9) cout << "total 100 koti operation done" << '\n';
+	}
 	return 0;
 }
 
-// This file is edited now by me. (porun someone). 
-// Objective is how to clonne/pull this to my local repository.
+
