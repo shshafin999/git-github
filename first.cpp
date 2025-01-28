4066da63b304220c1fc7a483f90161306d4f4feb
Shafayet Hossain Shafin <shshafin999@gmail.com>
2025-01-28 21:32:43 +0600

    writtern actual c++ code

--- first.cpp
+++ first.cpp
@@ -1,1 +1,9 @@
-hello from the other side. 
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+	int n; cin >> n;
+	cout << n << '\n';
+
+	cout << "shafayet Hossain shafin" << '\n';
+	return 0;
+}
